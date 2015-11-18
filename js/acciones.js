219ce4c073f52ecd1eaf03c12cd7e009a24a11da
lady-de-jesus <damcb164e12@gitthub.com>
2015-11-18 11:56:42 -0600

    subida2

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,25 @@
 // JavaScript Document
+function consulta()
+	{
+		datos="";
+		$.ajax({
+			type: "POST",
+			url: "192.168.1.30/procesar.php",
+			data: datos
+		}).done(function(mensaje){
+			alert(mensaje);
+		});
+	}
+
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
+	$("#btnconsulta").on("tap" ,function(){
+		alert ("preguntando...");
+		consulta();
+	}); 
 	
-}); 
+	
+	
+});
 });
 
